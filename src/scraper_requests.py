47b748f217dab246c86b38d92e79b78412b7ce1b
Jens Putzeys <jens.borlo@hotmail.be>
2021-05-21 18:30:00 +0200

    remove redundant cookies

--- src/scraper_requests.py
+++ src/scraper_requests.py
@@ -40,13 +40,6 @@ file_url = 'http://bonew.qteam.be/AnalyticalReporting/viewers/cdz_adv/downloadPD
 
 session.post(login_url, data=login_payload)
 
-# Set extra cookies (probably not necessary)
-session.cookies.set("infoview_css", "\"default.css\"")
-session.cookies.set("infoview_prefs", "\"view=0&dv4=1&defaultnav=folder&dv2=1&dv1=1&dv0=1&rpp=10\"")
-session.cookies.set("infoview_userLocale", "\"userDefaultLocale\"")
-session.cookies.set("infoview_userPreference", "\"maxOpageU=10;maxOpageUt=200;maxOpageC=10;tz=Europe/Paris;mUnit=inch;showFilters=true;smtpFrom=true;promptForUnsavedData=true;\"")
-session.cookies.set("infoview_userTimeZone", "\"Europe/Paris\"")
-
 def replace_url_encoding(element):
     return (element[0], urllib.parse.unquote(element[1]))
 
@@ -59,23 +52,13 @@ response = session.get(file_url, cookies=cookie_dict)
 # Problem: cookies don't work
 
 # How it works in terminal:
-# (some values removed for pushing to Github)
+# (cookies copied from request on website itself)
+# (values removed for pushing to Github)
+# The only necessary cookie is the JSESSIONID! (found through testing)
 """
 >>> import requests
 >>> file_url = 'http://bonew.qteam.be/AnalyticalReporting/viewers/cdz_adv/downloadPDForXLS.jsp?iViewerID=1&sEntry=we00020000aad681e98955&iReport=0&sPageMode=QuickDisplay&sReportMode=Analysis&iPage=1&zoom=100&isInteractive=false&iFoldPanel=0&doctype=wid&viewType=O&saveReport=N'
->>> cookies = { # Copied from real request on website
-...     "infoview_css": "\"default.css\"",
-...     "infoview_prefs": "\"view=0&dv4=1&defaultnav=folder&dv2=1&dv1=1&dv0=1&rpp=10\"",
-...     "infoview_userLocale": "\"userDefaultLocale\"",
-...     "infoview_userPreference": "\"maxOpageU=10;maxOpageUt=200;maxOpageC=10;tz=Europe/Paris;mUnit=inch;showFilters=true;smtpFrom=true;promptForUnsavedData=true;\"",
-...     "infoview_userTimeZone": "\"Europe/Paris\"",
-...     "InfoViewPLATFORMSVC_COOKIE_AUTH": "",
-...     "InfoViewPLATFORMSVC_COOKIE_CMS": "",
-...     "InfoViewPLATFORMSVC_COOKIE_TOKEN": "",
-...     "InfoViewPLATFORMSVC_COOKIE_USR": "",
-...     "InfoViewses": "",
-...     "ivsEntSessionVar": "\"\"",
-...     "ivsExitPage": "\"\"",
+>>> cookies = {
 ...     "JSESSIONID": ""
 ... }
 >>> 
@@ -83,3 +66,6 @@ response = session.get(file_url, cookies=cookie_dict)
 >>> response.status_code
 200
 """
+
+# The problem probably is that the JSESSIONID received at login is not the sid needed to ask for the file
+# Strange thing is that you don't get sid at login in response and you send a sid in the first login post
